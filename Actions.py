5e260617f1632c67ceff8737a63524386a6fa9d0
nanomoomug <nano@moomug.com>
2011-05-14 16:53:47 +0200

    Cleaned Actions file.

--- Actions.py
+++ Actions.py
@@ -10,54 +10,56 @@ __email__ = "nano@moomug.com"
 __status__ = "Just for fun!"
 
 class DeletionAction():
-    def __init__(self, internalState, path, filename, pos):
-        self.internalState = internalState
+    def __init__(self, internal_state, path, filename, pos):
+        self.internal_state = internal_state
         self.path = path
         self.filename = filename
         self.pos = pos
 
-    def undo(self, viewportSize):
+    def undo(self, viewport_size):
         shutil.move(self.path + '/discarded/' + self.filename,
                     self.path + '/' + self.filename)
-        self.internalState.add_image(self.path, self.filename,
-                                     self.pos, viewportSize)
-        self.internalState.jump_to_image(self.pos, viewportSize)
+        self.internal_state.add_image(self.path, self.filename,
+                                     self.pos, viewport_size)
+        self.internal_state.jump_to_image(self.pos, viewport_size)
 
-    def redo(self, viewportSize):
-        assert self.internalState.pos == self.pos
-        self.internalState.discard_current_image(viewportSize)
+    def redo(self, viewport_size):
+        assert self.internal_state.pos == self.pos
+        self.internal_state.discard_current_image(viewport_size)
         shutil.move(self.path + '/discarded/' + self.filename,
                     self.path + '/' + self.filename)
 
 class RotationAction():
-    def __init__(self, internalState, degrees, pos):
-        self.internalState = internalState
+    def __init__(self, internal_state, degrees, pos):
+        self.internal_state = internal_state
         self.degrees = degrees
         self.pos = pos
+        self.old_pos = None
 
-    def undo(self, viewportSize):
-        self.oldPos = self.internalState.pos
-        self.internalState.jump_to_image(self.pos, viewportSize)
-        self.internalState.rotate_current_image(-self.degrees, viewportSize)
+    def undo(self, viewport_size):
+        self.old_pos = self.internal_state.pos
+        self.internal_state.jump_to_image(self.pos, viewport_size)
+        self.internal_state.rotate_current_image(-self.degrees, viewport_size)
 
-    def redo(self, viewportSize):
-        assert self.internalState.pos == self.pos
-        self.internalState.rotate_current_image(self.degrees, viewportSize)
+    def redo(self, viewport_size):
+        assert self.internal_state.pos == self.pos
+        self.internal_state.rotate_current_image(self.degrees, viewport_size)
 
-        if self.oldPos != self.internalState.pos:
-            jumpAction = JumpAction(self.internalState, self.pos)
-            jumpAction.oldPos = self.oldPos
-            self.internalState.add_to_forward_history(jumpAction)
+        if self.old_pos != self.internal_state.pos:
+            jump_action = JumpAction(self.internal_state, self.pos)
+            jump_action.old_pos = self.old_pos
+            self.internal_state.add_to_forward_history(jump_action)
 
 class JumpAction():
-    def __init__(self, internalState, pos):
-        self.internalState = internalState
+    def __init__(self, internal_state, pos):
+        self.internal_state = internal_state
         self.pos = pos
+        self.old_pos = None
 
-    def undo(self, viewportSize):
-        self.oldPos = self.internalState.pos
-        self.internalState.jump_to_image(self.pos, viewportSize)
+    def undo(self, viewport_size):
+        self.old_pos = self.internal_state.pos
+        self.internal_state.jump_to_image(self.pos, viewport_size)
 
-    def redo(self, viewportSize):
-        assert self.internalState.pos == self.pos
-        self.internalState.jump_to_image(self.oldPos, viewportSize)
+    def redo(self, viewport_size):
+        assert self.internal_state.pos == self.pos
+        self.internal_state.jump_to_image(self.old_pos, viewport_size)
